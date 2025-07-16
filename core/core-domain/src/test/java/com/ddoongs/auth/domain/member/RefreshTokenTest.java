4b08518fa6fd87e38a16d49c4a69a6d2676ace80
김덕윤 <duk9741@gmail.com>
2025-07-16 23:40:27 +0900

    test: RefreshToken equals, hashcode 테스트

---
+++ core/core-domain/src/test/java/com/ddoongs/auth/domain/member/RefreshTokenTest.java
@@ -0,0 +1,24 @@
+package com.ddoongs.auth.domain.member;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.Instant;
+import java.util.UUID;
+import org.junit.jupiter.api.Test;
+
+class RefreshTokenTest {
+
+  @Test
+  void equalsAndHashCode() {
+    String jti = UUID.fromString("f47ac10b-58cc-4372-a567-0e02b2c3d479").toString();
+
+    RefreshToken refreshToken1 = new RefreshToken(
+        jti, "test@test.com", Instant.now().plusSeconds(10), "sample.refresh.token");
+
+    RefreshToken refreshToken2 = new RefreshToken(
+        jti, "test@test.com", Instant.now().plusSeconds(10), "sample.refresh.token");
+
+    assertThat(refreshToken1.equals(refreshToken2)).isTrue();
+    assertThat(refreshToken1.hashCode()).isEqualTo(refreshToken2.hashCode());
+  }
+}
