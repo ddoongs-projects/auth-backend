1436a58ce2e2495665c50cc5dcb8592685b2b020
김덕윤 <duk9741@gmail.com>
2025-07-24 19:24:31 +0900

    test: RestDocs 테스트에 Security 자동 설정 제외
    OAuth2 관련 테스트를 위해 Security와 OAuth2Client 자동 설정 제외

--- tests/api-docs/src/main/java/com/ddoongs/auth/restdocs/RestdocsTest.java
+++ tests/api-docs/src/main/java/com/ddoongs/auth/restdocs/RestdocsTest.java
@@ -1,10 +1,20 @@
 package com.ddoongs.auth.restdocs;
 
 import org.junit.jupiter.api.Tag;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.context.annotation.Import;
 
 @Tag("restdocs")
 @Import(RestdocsConfiguration.class)
+@ImportAutoConfiguration(
+    exclude = {
+      SecurityAutoConfiguration.class,
+      OAuth2ClientAutoConfiguration.class,
+    })
+@AutoConfigureMockMvc
 @AutoConfigureRestDocs
 public abstract class RestdocsTest {}
