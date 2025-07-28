b44e4ee052f9f230214a360fd48c7671685757b7
김덕윤 <duk9741@gmail.com>
2025-07-29 04:45:25 +0900

    chore: RedisConfig Value 이용하도록 수정

--- infrastructure/redis/src/main/java/com/ddoongs/auth/redis/config/RedisConfiguration.java
+++ infrastructure/redis/src/main/java/com/ddoongs/auth/redis/config/RedisConfiguration.java
@@ -5,9 +5,11 @@ import com.ddoongs.auth.domain.token.TokenExchange;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
@@ -16,9 +18,23 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfiguration {
 
+  @Value("${spring.data.redis.host}")
+  private String redisHost;
+
+  @Value("${spring.data.redis.port}")
+  private int redisPort;
+
+  @Value("${spring.data.redis.password}")
+  private String redisPassword;
+
   @Bean
   public RedisConnectionFactory redisConnectionFactory() {
-    return new LettuceConnectionFactory();
+    RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+    config.setHostName(redisHost);
+    config.setPort(redisPort);
+    config.setPassword(redisPassword);
+
+    return new LettuceConnectionFactory(config);
   }
 
   @Bean
